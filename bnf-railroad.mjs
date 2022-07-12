50e26b967b97679b5f4addb67905d093d6821968
Martin Dørum <martid0311@gmail.com>
2022-07-13 01:24:59 +0200

    initial commit

---
+++ bnf-railroad.mjs
@@ -0,0 +1,101 @@
+import {parseBnf, StringReader} from './bnf.mjs';
+import * as rr from './vendor/railroad.mjs';
+import * as fs from 'fs';
+
+function createRailroad(node) {
+	if (node.seq) {
+		return new rr.Sequence(...node.seq.map(createRailroad));
+	} else if (node.any) {
+		return new rr.Choice(0, ...node.any.map(createRailroad));
+	} else if (node.terminal) {
+		return new rr.Terminal(node.terminal);
+	} else if (node.ref) {
+		return new rr.NonTerminal(node.ref);
+	} else if (node.special) {
+		return new rr.NonTerminal("[" + node.special + "]");
+	} else if (node.optional) {
+		return new rr.Optional(createRailroad(node.optional));
+	} else if (node.repeat0) {
+		return new rr.ZeroOrMore(createRailroad(node.repeat0));
+	} else if (node.repeat1) {
+		return new rr.OneOrMore(createRailroad(node.repeat1));
+	} else {
+		console.warn("Bad node:", node);
+		return new rr.Terminal("");
+	}
+}
+
+let argv = process.argv.slice(2);
+if (argv.length != 2) {
+	console.log("Usage: bnf-railroad <infile> <outfile>");
+	process.exit(1);
+}
+
+let text = fs.readFileSync(argv[0], "utf-8");
+let outfile = fs.createWriteStream(argv[1]);
+let r = new StringReader(text);
+let grammar = parseBnf(r);
+
+outfile.write(`<!DOCTYPE html>
+<html>
+<body>
+<style>
+h2 {
+    margin-bottom: 0px;
+}
+code pre {
+    margin-top: 10px;
+}
+svg.railroad-diagram path {
+    stroke-width: 3;
+    stroke: black;
+    fill: rgba(0,0,0,0);
+}
+svg.railroad-diagram text {
+    font: bold 14px monospace;
+    text-anchor: middle;
+    white-space: pre;
+}
+svg.railroad-diagram text.diagram-text {
+    font-size: 12px;
+}
+svg.railroad-diagram text.diagram-arrow {
+    font-size: 16px;
+}
+svg.railroad-diagram text.label {
+    text-anchor: start;
+}
+svg.railroad-diagram text.comment {
+    font: italic 12px monospace;
+}
+svg.railroad-diagram g.non-terminal text {
+    /*font-style: italic;*/
+}
+svg.railroad-diagram rect {
+    stroke-width: 3;
+    stroke: black;
+    fill: hsl(120,100%,90%);
+}
+svg.railroad-diagram rect.group-box {
+    stroke: gray;
+    stroke-dasharray: 10 5;
+    fill: none;
+}
+svg.railroad-diagram path.diagram-text {
+    stroke-width: 3;
+    stroke: black;
+    fill: white;
+    cursor: help;
+}
+svg.railroad-diagram g.diagram-text:hover path.diagram-text {
+    fill: #eee;
+}
+</style>\n`);
+for (let key of Object.keys(grammar)) {
+	outfile.write(`<div>\n<h2>${key}</h2>\n`);
+	outfile.write(new rr.Diagram(createRailroad(grammar[key])).toString());
+	outfile.write("\n</div>\n");
+}
+outfile.write(`<div>\n<h2>BNF:</h2>\n<code><pre>${text}</pre></code>\n</div>\n`);
+outfile.write("</body>\n</html>\n");
+outfile.close();
---
+++ bnf.mjs
@@ -0,0 +1,237 @@
+export class StringReader {
+	constructor(str) {
+		this.str = str;
+		this.index = 0;
+		this.lineStart = false;
+		this.line = 1;
+		this.col = 1;
+	}
+
+	peek() {
+		if (this.lineStart) {
+			return 'LINE';
+		}
+
+		if (this.index >= this.str.length) {
+			return null;
+		} else {
+			return this.str[this.index];
+		}
+	}
+
+	consume() {
+		if (this.lineStart) {
+			this.lineStart = false;
+		} else {
+			this.increment();
+		}
+	}
+
+	increment() {
+		let ch = this.str[this.index];
+		this.index += 1;
+		if (ch == '\n') {
+			this.col = 1;
+			this.line += 1;
+		} else {
+			this.col += 1;
+		}
+	}
+
+	skipSpace() {
+		if (this.lineStart) {
+			return;
+		}
+
+		while (true) {
+			let ch = this.str[this.index];
+			if (ch == '\n' || ch == '\r') {
+				this.lineStart = true;
+			} else if (ch == ' ' || ch == '\t') {
+				this.lineStart = false;
+			} else {
+				break;
+			}
+
+			this.increment();
+		}
+	}
+
+	skip(str) {
+		for (let ch of str) {
+			if (this.peek() != ch) {
+				this.error("Expected '" + str + "'");
+			}
+			this.consume();
+		}
+	}
+
+	error(err) {
+		throw new Error(err + " -- at " + this.line + ":" + this.col);
+	}
+}
+
+function readIdent(r) {
+	let str = "";
+	while (true) {
+		let ch = r.peek();
+		if (ch == null || !/^[a-zA-Z0-9\-_]$/.test(ch)) {
+			if (str.length == 0) {
+				r.error("Zero-length identifier before '" + ch + "'");
+			}
+			return str;
+		}
+
+		str += ch;
+		r.consume();
+	}
+}
+
+function readString(r, end) {
+	let str = "";
+	while (true) {
+		let ch = r.peek();
+		if (ch == null) {
+			r.error("Unexpected EOF");
+		}
+
+		if (ch == end) {
+			r.consume();
+			return str;
+		} else if (ch == '\\') {
+			r.consume();
+			ch = r.peek();
+			if (ch == null) {
+				r.error("Unexpected EOF");
+			}
+
+			if (ch == 'n') {
+				str += '\n';
+			} else if (ch == 'r') {
+				str += '\r';
+			} else if (ch == end || ch == '\\' || ch == '.' || ch == '-') {
+				str += ch;
+			} else {
+				r.error("Unexpected escape character: '" + ch + "'");
+			}
+
+			r.consume();
+		} else {
+			str += ch;
+			r.consume();
+		}
+	}
+}
+
+function parseExpressionPart(r) {
+	let ch = r.peek();
+	let expr;
+	if (ch == '(') {
+		r.consume();
+		expr = parseExpression(r);
+		r.skipSpace();
+		r.skip(')');
+	} else if (ch == '"') {
+		r.consume();
+		expr = {terminal: readString(r, '"')};
+	} else if (ch == "'") {
+		r.consume();
+		expr = {terminal: readString(r, "'")};
+	} else if (ch == '[') {
+		r.consume();
+		expr = {special: readString(r, "]")};
+	} else {
+		expr = {ref: readIdent(r)};
+	}
+
+	while (true) {
+		r.skipSpace();
+		ch = r.peek();
+		if (ch == '*') {
+			r.consume();
+			expr = {repeat0: expr};
+		} else if (ch == '+') {
+			r.consume();
+			expr = {repeat1: expr};
+		} else if (ch == '?') {
+			r.consume();
+			expr = {optional: expr};
+		} else {
+			return expr;
+		}
+	}
+}
+
+function parseExpressionSequence(r) {
+	let parts = [];
+	while (true) {
+		r.skipSpace();
+		let ch = r.peek();
+		if (ch == 'LINE' || ch == null || ch == ')' || ch == '|') {
+			break;
+		}
+
+		parts.push(parseExpressionPart(r));
+	}
+
+	if (parts.length == 1) {
+		return parts[0];
+	} else if (parts.length == 0) {
+		return {};
+	} else {
+		return {seq: parts};
+	}
+}
+
+function parseExpression(r) {
+	let parts = [];
+	while (true) {
+		r.skipSpace();
+		let ch = r.peek();
+		if (ch == 'LINE' || ch == null || ch == ')' || ch == '|') {
+			break;
+		} else {
+			parts.push(parseExpressionSequence(r));
+		}
+
+		r.skipSpace();
+		if (r.peek() == '|') {
+			r.consume();
+		}
+	}
+
+	if (parts.length == 1) {
+		return parts[0];
+	} else if (parts.length == 0) {
+		return {};
+	} else {
+		return {any: parts};
+	}
+}
+
+function parseDecl(r) {
+	let name = readIdent(r);
+	r.skipSpace();
+	r.skip("::=");
+	r.skipSpace();
+	let expr = parseExpression(r);
+	if (r.peek() == 'LINE') {
+		r.consume();
+	}
+	return [name, expr];
+}
+
+export function parseBnf(r) {
+	let decls = {};
+	while (true) {
+		r.skipSpace();
+		if (r.peek() == null) {
+			break;
+		}
+
+		let [name, expr] = parseDecl(r);
+		decls[name] = expr;
+	}
+
+	return decls;
+}
