112bf2e4f4eaa7d55153670680d9113f30fb7002
Martin Dørum <martid0311@gmail.com>
2022-07-13 23:57:17 +0200

    add 'export to png' button to the generated website

--- bin/bnf-railroad.mjs
+++ bin/bnf-railroad.mjs
@@ -45,66 +45,139 @@ function sanitizeHtml(text) {
 		.replace(/>/g, "&gt;");
 }
 
+let svgcss = `
+<style>
+path {
+	stroke-width: 3;
+	stroke: black;
+	fill: rgba(0,0,0,0);
+}
+text {
+	font: bold 13px monospace;
+	text-anchor: middle;
+	white-space: pre;
+}
+text.diagram-text {
+	font-size: 12px;
+}
+text.diagram-arrow {
+	font-size: 16px;
+}
+text.label {
+	text-anchor: start;
+}
+text.comment {
+	font: italic 12px monospace;
+}
+g.non-terminal text {
+	/*font-style: italic;*/
+}
+rect {
+	stroke-width: 3;
+	stroke: black;
+	fill: hsl(120,100%,90%);
+}
+rect.group-box {
+	stroke: gray;
+	stroke-dasharray: 10 5;
+	fill: none;
+}
+path.diagram-text {
+	stroke-width: 3;
+	stroke: black;
+	fill: white;
+	cursor: help;
+}
+path.diagram-text {
+	fill: #eee;
+}
+</style>`;
+
 outfile.write(`<!DOCTYPE html>
 <html>
 <body>
 <style>
+#container {
+	display: inline-block;
+}
 h2 {
-    margin-top: 0px;
-    margin-bottom: -10px;
+	margin-top: 0px;
+	margin-bottom: -10px;
+	font-family: Sans-Serif;
+	font-size: 24px;
 }
 code pre {
-    margin-top: 10px;
+	margin-top: 10px;
 }
-svg.railroad-diagram path {
-    stroke-width: 3;
-    stroke: black;
-    fill: rgba(0,0,0,0);
+</style>
+<script>
+function svgToImg(svg, cb) {
+	var img = document.createElement("img");
+	var xml = new XMLSerializer().serializeToString(svg);
+	img.src = "data:image/svg+xml;base64," + btoa(xml);
+	img.onload = function() { cb(img); };
 }
-svg.railroad-diagram text {
-    font: bold 13px monospace;
-    text-anchor: middle;
-    white-space: pre;
+
+function pageToCanvas(cb) {
+	var diagrams = document.querySelectorAll(".diagram");
+	var width = 0;
+	var height = 0;
+	for (var i = 0; i < diagrams.length; ++i) {
+		var diag = diagrams[i];
+		if (diag.offsetWidth > width) {
+			width = diag.offsetWidth;
+		}
+		height += diag.offsetHeight;
+	}
+	width += 16;
+	height += 16;
+
+	var canvas = document.createElement("canvas");
+	canvas.width = width * 2;
+	canvas.height = height * 2;
+	var ctx = canvas.getContext("2d");
+	ctx.scale(2, 2);
+
+	ctx.fillStyle = "white";
+	ctx.fillRect(0, 0, width, height);
+
+	var left = diagrams.length;
+
+	ctx.fillStyle = "black";
+	ctx.textBaseline = "top";
+	ctx.font = "bold 24px Sans-Serif";
+	var y = 8;
+	for (var i = 0; i < diagrams.length; ++i) {
+		var diag = diagrams[i];
+		var title = diag.querySelector("h2").innerText;
+		svgToImg(diag.querySelector("svg"), function(y, title, img) {
+			ctx.fillText(title, 8, y);
+			ctx.drawImage(img, 8, y + 15);
+			left -= 1;
+			if (left == 0) {
+				cb(canvas);
+			}
+		}.bind(null, y, title));
+		y += diag.offsetHeight;
+	}
 }
-svg.railroad-diagram text.diagram-text {
-    font-size: 12px;
+
+function saveImage() {
+	pageToCanvas(function(canvas) {
+		location.href = canvas.toDataURL("image/png");
+	});
 }
-svg.railroad-diagram text.diagram-arrow {
-    font-size: 16px;
-}
-svg.railroad-diagram text.label {
-    text-anchor: start;
-}
-svg.railroad-diagram text.comment {
-    font: italic 12px monospace;
-}
-svg.railroad-diagram g.non-terminal text {
-    /*font-style: italic;*/
-}
-svg.railroad-diagram rect {
-    stroke-width: 3;
-    stroke: black;
-    fill: hsl(120,100%,90%);
-}
-svg.railroad-diagram rect.group-box {
-    stroke: gray;
-    stroke-dasharray: 10 5;
-    fill: none;
-}
-svg.railroad-diagram path.diagram-text {
-    stroke-width: 3;
-    stroke: black;
-    fill: white;
-    cursor: help;
-}
-svg.railroad-diagram g.diagram-text:hover path.diagram-text {
-    fill: #eee;
-}
-</style>\n`);
+</script>
+<button style="float: right" onclick="saveImage()">Export to PNG</button>
+`);
+outfile.write(`<div id="container">\n`);
 for (let key of Object.keys(grammar)) {
-	outfile.write(`<div>\n<h2>${sanitizeHtml(key)}</h2>\n`);
-	outfile.write(new rr.Diagram([createRailroad(grammar[key])]).toString());
+	outfile.write(`<div class="diagram">\n<h2>${sanitizeHtml(key)}</h2>\n`);
+	let svg = new rr.Diagram([createRailroad(grammar[key])]).toString();
+	svg = svg.replace('>', '>' + svgcss);
+	outfile.write(svg);
 	outfile.write("\n</div>\n");
 }
+outfile.write("</div>");
 outfile.write("</body>\n</html>\n");
 outfile.close();
